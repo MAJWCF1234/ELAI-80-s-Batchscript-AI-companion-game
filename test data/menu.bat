f5be75fc8d64ab0285ef5ade01d34beb25b3b67f
MAJWCF1234 <126084222+MAJWCF1234@users.noreply.github.com>
2023-05-18 10:43:51 -0500

    Add files via upload

---
+++ test data/menu.bat
@@ -0,0 +1,18 @@
+@echo off
+:MENU
+cls
+echo.
+echo ==================================================
+echo ^|^|                                              ^|^|
+echo ^|^|       SELECT YOUR TASK BY NUMBER             ^|^|
+echo ^|^|                                              ^|^|
+echo ==================================================
+echo ^|^| 1 - Play a game                              ^|^|
+echo ^|^| 2 - Have a conversation                      ^|^|
+echo ^|^| 3 - Tool Menu                                ^|^|
+echo ^|^| 4 - Exit                                     ^|^|
+echo ==================================================
+echo.
+pause
+cls
+pause
